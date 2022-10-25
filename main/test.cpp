e103b7ced23380033ddd94903edfd34359c5aac7
alancotal <acotal2018@inf.udec.cl>
2022-10-24 22:59:34 -0300

    draw

--- main/test.cpp
+++ main/test.cpp
@@ -2,8 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <map>
-#include <iostream>
-#include <fstream>
+#include <sstream>
 
 using namespace std;
 using namespace cimg_library;
@@ -17,16 +16,27 @@ int main(int argc, char *argv[])
     CImg<float> src1;
     src1.load_tiff(argv[1]);
     src1.crop(1000,1000,0,0,3700,3700,0,0,3);
-    const unsigned char color[] = { 255,0,0 };
+    const unsigned char color[] = { 255,50,10 };
     src1.draw_point(3,1,color);
     cout<<src1.size()<<endl;
 
-    src1.display();
+    //src1.display();
     int w1 = src1.width();
     int h1 = src1.height();
     cout<<src1.atXY(3,1,0,0)<<endl;
-    
-   
+    CImg<float> tar(w1,h1,1,3,0);
+
+    ifstream file1;
+    file1.open("./final.txt", ios_base::in);
+    for(string line1; getline(file1, line1); ){
+        istringstream in(line1); 
+        float x,y;
+        in>>x>>y;
+
+        tar.draw_point(x,y,color);
+    }
+    file1.close();
+    (src1,tar).display();
     /*
      ofstream file;
     file.open("./archivo.txt");
